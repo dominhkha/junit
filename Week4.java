23216608fccdeb82ce0727b0e925157700072f53
dominhkha <kha.minhdo199@gmail.com>
2022-07-22 08:34:56 +0700

    init commit

---
+++ Week4.java
@@ -0,0 +1,13 @@
+public class Week4 {
+    public static int max2Int(int a, int b){
+       
+    }
+    public static int minArray(int[] arr) {
+        
+
+    }
+    public static String calculateBMI(double weight, double height) {
+        
+    }
+
+}
---
+++ Week4Test.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Week4Test {
+    @Test
+    public void testMax2Int1() {
+
+    }
+    @Test
+    public void testMax2Int2() {
+
+    }
+    @Test
+    public void testMinArray1() {
+
+    }
+    @Test
+    public void testMinArray2() {
+
+    }
+    @Test
+    public void testCalculateBMI1() {
+
+
+    }
+    @Test
+    public void testCalculateBMI2() {
+
+
+    }
+}
